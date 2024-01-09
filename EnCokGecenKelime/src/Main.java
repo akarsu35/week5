913043a274662683e541c7bfc0ed4b11810a6a4c
akarsu35 <cumhurakarsu@gmail.com>
2024-01-09 23:25:23 +0300

    commit1

---
+++ EnCokGecenKelime/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Bir cümle giriniz : ");
+        String cumle=sc.nextLine();
+
+        String temizCumle = cumle.replaceAll("[.,!;:?]", "");//cümleyi bazı noktalama işaretlerinden arındırırız.
+
+        // Sonucu ekrana yazdırma
+        System.out.println("Temizlenmiş Cümle: " + temizCumle);
+        System.out.println("----------------");
+
+        String[] kelimeler=temizCumle.split(" ");//girilen cümleyi " " kararkterlerinden böleriz.
+
+        HashMap<String,Integer> kelimeSayilari=new HashMap<>();
+        for (String kelime : kelimeler) {
+            // Küçük/büyük harf duyarlı olmadan kelimeleri kontrol ederiz.
+            String kucukHarfKelime = kelime.toLowerCase();
+
+            // Eğer kelime zaten map'te varsa, sayacı bir arttırırız
+            // Yoksa, yeni bir giriş yaparız ve sayacı 1 olarak ayarlarız
+            kelimeSayilari.put(kucukHarfKelime, kelimeSayilari.getOrDefault(kucukHarfKelime,0)+1);
+        }
+        // Sonuçları ekrana yazdırırız
+        for (String kelime : kelimeSayilari.keySet()) {
+            System.out.println(kelime + " : " + kelimeSayilari.get(kelime));
+        }
+
+    }
+}
